b5108dbd02a40a9b62cdb502923e68e042023855
ourahmamaroua <122113141+ourahma@users.noreply.github.com>
2025-05-24 17:18:13 +0000

    fin demo

--- client-soap-java/src/main/java/net/ourahma/Main.java
+++ client-soap-java/src/main/java/net/ourahma/Main.java
@@ -2,10 +2,20 @@ package net.ourahma;
 
 import proxy.BanqueService;
 import proxy.BanqueWS;
+import proxy.Compte;
 
 public class Main {
     public static void main(String[] args) {
         BanqueService proxy = new BanqueWS().getBanqueServicePort();
         System.out.println("Mathode conversion EuroToDH :"+proxy.conversionEuroToDH(98));
+        System.out.println("***************Consulter un compte ***************************");
+        Compte compte = proxy.getCompte(1);
+        System.out.println(compte.getCode());
+        System.out.println(compte.getSolde());
+        System.out.println(compte.getDateCreation());
+        System.out.println("***************Consulter la liste des comptes ***************************");
+        proxy.listComptes().forEach(c ->{
+            System.out.println("Code : "+c.getCode()+"Solde : "+c.getSolde()+"DateCreation : "+c.getDateCreation());
+        });
     }
 }
